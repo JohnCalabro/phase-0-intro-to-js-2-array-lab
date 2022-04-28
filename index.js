fd9b7d12cc3c1ac4758d3b3ed0081e9b4aae8774
johncalabro <j.calabro4@gmail.com>
2022-04-27 20:10:28 -0400

    commit tree 0f9c9958cac3cf98728f03d69b70a2181d8ce023

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+const cats = ['Milo' , 'Otis' , 'Garfield']
+
+function destructivelyAppendCat(name){
+    cats.push('Ralph')
+}
+
+cats
+
+function destructivelyPrependCat(name){
+    cats.unshift('Bob')
+}
+cats
+
+function destructivelyRemoveLastCat(){
+    cats.pop('Garfield')
+}
+cats
+
+function destructivelyRemoveFirstCat(){
+    cats.shift('Milo')
+}
+cats
+
+
+function appendCat(name){
+  const  copyOfCats = [...cats, name]
+  return copyOfCats
+}
+
+function prependCat(name){
+    const  copyOfCats = [name, ...cats]
+    return copyOfCats 
+}
+
+function removeLastCat(){
+    cats.slice(0,-1)
+    cats.slice
+}
+
+
+
+
+// function removeFirstCat(){
+    // cats.slice(1)
+// }
+// cats
